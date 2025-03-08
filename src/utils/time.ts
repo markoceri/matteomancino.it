240b80ce260f242cc4efaa106246264716ea843f
Marco Mancino <mancinomarko@hotmail.it>
2025-03-08 16:15:00 +0100

    aggiornamento cumulativo

--- src/utils/time.ts
+++
@@ -1,8 +0,0 @@
-export const formatDuration = (duration: number): string => {
-  return new Intl.DateTimeFormat("it-IT", {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZone: "UTC",
-  }).format(new Date(Math.round(duration) * 1000));
-};
